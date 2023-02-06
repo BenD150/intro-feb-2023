7054d75256111ca7b7a31ec4a23f232a354f1978
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-06 15:41:27 -0500

    HTTP GET and POST requests

--- Day1/Finger/Program.cs
+++ Day1/Finger/Program.cs
@@ -1,19 +1,19 @@
 ﻿using Finger;
 
+var builder = WebApplication.CreateBuilder(args);
 
-// Using the System namespace, Console class, WriteLine method to print out a line
-// Shift+Alt+F formats code for you!
-Console.Write("What is your status: ");
-string? status = Console.ReadLine();
 
-//Type identifier = new 
-// myStatus is an instance of StatusMessage
-if (status != null)
-{
-    StatusMessage myStatus = new StatusMessage(status, DateTimeOffset.Now);
-    Console.WriteLine($"You said your status was {myStatus.Status} at {myStatus.When:T}");
-}
-else 
-{
-    Console.WriteLine("Sorry, cannot have a null status");
-}
+var app = builder.Build();
+
+app.MapGet("/status", () => {
+    var status = new StatusMessage("All is Good!", DateTimeOffset.Now);
+    return status;
+});
+
+app.MapPost("/status", (StatusRequest req) => {
+    return req;
+});
+
+app.Run(); // Blocking call
+
+public record StatusRequest(string Message);
