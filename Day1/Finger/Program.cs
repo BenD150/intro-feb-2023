2e51d41bb57a95e4ed8c493a66468457f866869f
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-06 15:05:51 -0500

    Worked on status message

---
+++ Day1/Finger/Program.cs
@@ -0,0 +1,19 @@
+﻿using Finger;
+
+
+// Using the System namespace, Console class, WriteLine method to print out a line
+// Shift+Alt+F formats code for you!
+Console.Write("What is your status: ");
+string? status = Console.ReadLine();
+
+//Type identifier = new 
+// myStatus is an instance of StatusMessage
+if (status != null)
+{
+    StatusMessage myStatus = new StatusMessage(status, DateTimeOffset.Now);
+    Console.WriteLine($"You said your status was {myStatus.Status} at {myStatus.When:T}");
+}
+else 
+{
+    Console.WriteLine("Sorry, cannot have a null status");
+}
---
+++ Day1/Finger/StatusMessage.cs
@@ -0,0 +1,5 @@
+namespace Finger
+{
+// Essentially just a class 
+public record StatusMessage(string Status, DateTimeOffset When);
+}
