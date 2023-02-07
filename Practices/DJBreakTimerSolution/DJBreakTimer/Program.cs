486d2af26ffba859344372fc49532bd5cfc4138e
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-07 15:28:04 -0500

    Minute Countdown Timer

--- Practices/DJBreakTimerSolution/DJBreakTimer/Program.cs
+++ Practices/DJBreakTimerSolution/DJBreakTimer/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 
-
+// Iteration 1 
 Console.Write("How many minutes is the break? \n");
 var breakTime = Console.ReadLine();
 var breakAsDouble = double.Parse(breakTime);
@@ -15,6 +15,18 @@ var totalMinutes = ts.TotalMinutes;
 var endBreakTime = DateTimeOffset.Now.AddMinutes(breakAsDouble);
 Console.Write($"At the end of the break, it will be {endBreakTime}\n\n");
 
-Console.Write($"The user should start the song at {endBreakTime.AddSeconds(totalMinutes * -1)}");
+Console.Write($"The user should start the song at {endBreakTime.AddSeconds(totalMinutes * -1)}\n\n");
+
+
+
+// Iteration 2
+var timeRemaining = breakAsDouble;
+while (timeRemaining > 0)
+{
+    Console.Write($"There are {timeRemaining} minutes left in the break\n");
+    Thread.Sleep(1000);
+    timeRemaining -= 1;
+}
+
 
 
