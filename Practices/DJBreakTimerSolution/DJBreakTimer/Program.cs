ef1409024958fcd3e9eec84cd8f994a4202c1bdf
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-07 15:06:46 -0500

    Iteration 1

--- Practices/DJBreakTimerSolution/DJBreakTimer/Program.cs
+++ Practices/DJBreakTimerSolution/DJBreakTimer/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+
+Console.Write("How many minutes is the break? \n");
+var breakTime = Console.ReadLine();
+var breakAsDouble = double.Parse(breakTime);
+
+
+Console.Write("How long is the song? \n");
+var songLength = Console.ReadLine();
+var ts = TimeSpan.Parse(songLength);
+var totalMinutes = ts.TotalMinutes;  
+
+
+var endBreakTime = DateTimeOffset.Now.AddMinutes(breakAsDouble);
+Console.Write($"At the end of the break, it will be {endBreakTime}\n\n");
+
+Console.Write($"The user should start the song at {endBreakTime.AddSeconds(totalMinutes * -1)}");
+
+
