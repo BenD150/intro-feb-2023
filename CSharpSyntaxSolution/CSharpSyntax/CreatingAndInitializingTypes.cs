3dbc715ffc0d01bb24f4e483b071cf691f626493
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-07 11:47:03 -0500

    More type unit tests

--- CSharpSyntaxSolution/CSharpSyntax/CreatingAndInitializingTypes.cs
+++ CSharpSyntaxSolution/CSharpSyntax/CreatingAndInitializingTypes.cs
@@ -3,6 +3,27 @@
 public class CreatingAndInitializingTypes
 {
     [Fact]
+    public void UsingLiteralsToCreateInstancesOfTypes()
+    {
+        // local variables -- variables that are declared inside a method, or property.
+        string myName = "Jeff"; // Initializing
+        char mi = 'M';
 
+        bool isVendor = true;
+        // delete me
+        int myAge = 53;
+        bool isLegalAgeToDrink = myAge >= 21;
 
+        Taco food = new Taco();
+        Assert.Equal("Jeff", myName);
+        Assert.Equal(53, myAge);
+    }
+
+    [Fact]
+    public void ImplicitlyTypedLocalVariables()
+    {
+
+    }
 }
+
+public class Taco { }
