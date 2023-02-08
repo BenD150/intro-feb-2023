05d5edd1e3d9db89417c289699d2df6e231b5ca1
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-08 14:47:45 -0500

    Kata parts 1, 2 and 3

--- Practices/stringcalculator-wednesday1/StringCalculator.cs
+++ Practices/stringcalculator-wednesday1/StringCalculator.cs
@@ -1,11 +1,36 @@
 ﻿
+using System.Runtime.InteropServices;
+
 namespace StringCalculator;
 
 public class StringCalculator
 {
 
+    // This public method will be the only thing we are going to test
     public int Add(string numbers)
     {
-        return -42;
+        string[] nums = { };
+
+        if (numbers != "")
+        {
+            nums = numbers.Split(',', '\n');
+        }
+
+        if (nums.Length == 0) 
+        { 
+            return 0;   
+
+        } else
+        {
+            int sum = 0;
+            int numberAsInt = 0;
+            foreach (string number in nums) 
+            { 
+                numberAsInt = int.Parse(number);
+                sum += numberAsInt; 
+            }
+            return sum;
+        }
     }
+
 }
