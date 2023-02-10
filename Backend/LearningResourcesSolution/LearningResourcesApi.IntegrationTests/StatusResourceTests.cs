303f3c19b10dd42aaa708f1a52eb76e96d4706b9
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-10 15:49:28 -0500

    First part of integration test

---
+++ Backend/LearningResourcesSolution/LearningResourcesApi.IntegrationTests/StatusResourceTests.cs
@@ -0,0 +1,16 @@
+﻿namespace LearningResourcesApi.IntegrationTests;
+
+public class StatusResourceTests
+{
+    [Fact]
+    public async Task TheStatusResource()
+    {
+        await using var host = await AlbaHost.For<Program>();
+
+        await host.Scenario(api => // Integration test - usually has many steps
+        {
+            api.Get.Url("/status");
+            api.StatusCodeShouldBeOk(); // 200 status code
+        });
+    }
+}
