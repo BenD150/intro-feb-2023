141f9b5acdb702c056ca2e9c46085c19cce64065
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-10 16:08:15 -0500

    Week 1 finished

--- Backend/LearningResourcesSolution/LearningResourcesApi.IntegrationTests/StatusResourceTests.cs
+++ Backend/LearningResourcesSolution/LearningResourcesApi.IntegrationTests/StatusResourceTests.cs
@@ -1,4 +1,6 @@
-﻿namespace LearningResourcesApi.IntegrationTests;
+﻿using LearningResourcesApi.Controllers;
+
+namespace LearningResourcesApi.IntegrationTests;
 
 public class StatusResourceTests
 {
@@ -7,10 +9,16 @@ public class StatusResourceTests
     {
         await using var host = await AlbaHost.For<Program>();
 
-        await host.Scenario(api => // Integration test - usually has many steps
+        var response = await host.Scenario(api => // Integration test - usually has many steps
         {
             api.Get.Url("/status");
             api.StatusCodeShouldBeOk(); // 200 status code
         });
+
+        var responseMessage = response.ReadAsJson<GetStatusResponse> ();
+
+        Assert.NotNull(responseMessage);
+        Assert.Equal("All Good!", responseMessage.Message);
+        Assert.Equal("555-555-5555", responseMessage.Contact);
     }
 }
--- Backend/LearningResourcesSolution/LearningResourcesApi/Controllers/StatusController.cs
+++ Backend/LearningResourcesSolution/LearningResourcesApi/Controllers/StatusController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using LearningResourcesApi.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.OpenApi.Models;
 
@@ -6,10 +7,20 @@ namespace LearningResourcesApi.Controllers;
 
 public class StatusController : ControllerBase
 {
+    private ISystemTime _systemTime;
+
+    public StatusController(ISystemTime systemTime)
+    {
+        _systemTime = systemTime;
+    }
+
     [HttpGet("/status")]
     public ActionResult GetTheStatus()
     {
         // Look this up from a database for example
+        // If it is between midnight and 4PM use a phone number
+        // outside of that, use an email address
+        var contact = _systemTime.GetCurrent().Hour < 4 ? "bob@aol.com" : "555 555-5555";
         var response = new GetStatusResponse
         {
             Message = "All Good!",
