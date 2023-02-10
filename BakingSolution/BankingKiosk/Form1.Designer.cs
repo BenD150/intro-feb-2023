992b3de9fce3ea3af217d3cb6700e75432f4b2fa
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-10 11:29:11 -0500

    lambda function test

--- BakingSolution/BankingKiosk/Form1.Designer.cs
+++ BakingSolution/BankingKiosk/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.amountInput = new System.Windows.Forms.TextBox();
             this.depositButton = new System.Windows.Forms.Button();
             this.withdrawButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // amountInput
@@ -48,6 +49,7 @@
             this.depositButton.TabIndex = 1;
             this.depositButton.Text = "Deposit";
             this.depositButton.UseVisualStyleBackColor = true;
+            this.depositButton.Click += new System.EventHandler(this.depositButton_Click);
             // 
             // withdrawButton
             // 
@@ -57,12 +59,24 @@
             this.withdrawButton.TabIndex = 2;
             this.withdrawButton.Text = "Withdraw";
             this.withdrawButton.UseVisualStyleBackColor = true;
+            this.withdrawButton.Click += new System.EventHandler(this.withdrawButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(377, 295);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.withdrawButton);
             this.Controls.Add(this.depositButton);
             this.Controls.Add(this.amountInput);
@@ -78,5 +92,6 @@
         private TextBox amountInput;
         private Button depositButton;
         private Button withdrawButton;
+        private Button button1;
     }
 }
--- BakingSolution/BankingKiosk/Form1.cs
+++ BakingSolution/BankingKiosk/Form1.cs
@@ -1,10 +1,54 @@
-namespace BankingKiosk
+using Banking.Domain;
+
+namespace BankingKiosk;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    BankAccount _account;
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+        _account = new BankAccount(new StandardBonusCalculator(new StandardBusinessClock(new SystemTime())));
+        UpdateBalanceDisplay();
+    }
+
+    private void UpdateBalanceDisplay()
+    {
+        this.Text = $"You have a balance of {_account.GetBalance():c} currently";
+    }
+
+    private void depositButton_Click(object sender, EventArgs e)
+    {
+        DoTransaction(_account.Deposit);
+
+    }
+
+    private void withdrawButton_Click(object sender, EventArgs e)
+    {
+        DoTransaction(_account.Withdraw); // Named Method ("Method Group")
+
+    }
+
+    // Example of using a delegate
+    private void DoTransaction(Action<decimal> op) // If you call me you have to give me a method that returns void and takes a decimal argument
+    {
+        var amount = decimal.Parse(amountInput.Text);
+        op(amount);
+        UpdateBalanceDisplay();
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+        // Anonymous function (a method with no name) we call these "Lambdas" in C#
+        DoTransaction((amount) => MessageBox.Show("You clicked a button, blah" + amount.ToString()));
+
+        // "Anonymous Delegate" - how we did it until about 2008
+        /*
+         * DoTransaction (delegate (decimal amount)
+         * {
+         *  MessageBox.Show("You clicked a button, blah" + amount.ToString()
+         *  });
+         *  HomoIconicity - code and data structures are represented the same way
+         */
     }
 }
