a0f72d59eb55bbaf82996175d7ada3e24f490032
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-10 15:37:44 -0500

    Starting work on API

---
+++ Backend/LearningResourcesSolution/LearningResourcesApi/Controllers/StatusController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Models;
+
+namespace LearningResourcesApi.Controllers;
+
+public class StatusController : ControllerBase
+{
+    [HttpGet("/status")]
+    public ActionResult GetTheStatus()
+    {
+        // Look this up from a database for example
+        var response = new GetStatusResponse
+        {
+            Message = "All Good!",
+            Contact = "555-555-5555"
+        };
+        return Ok(response);
+    }
+}
+
+
+public class GetStatusResponse
+{
+    public string Message { get; set; } = string.Empty;
+    public string Contact { get; set; } = string.Empty;
+}
