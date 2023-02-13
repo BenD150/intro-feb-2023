b0861aa449a11c5f808dc22e4a8586fb86dfae52
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-13 15:19:06 -0500

    even more progress on shoppinglist

--- frontend/src/app/components/shopping-list/shopping-list.component.ts
+++ frontend/src/app/components/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-shopping-list',
@@ -7,14 +8,38 @@ import { Component } from '@angular/core';
 })
 export class ShoppingListComponent {
 
+  form = new FormGroup({
+    item: new FormControl<string>('', { nonNullable: true})
+  
+})
+
   items: ShoppingItem[] = [
     { description: 'Beer', purchased: false },
     { description: 'Buns', purchased: false },
     { description: 'Eggs', purchased: true },
   ]
 
+  clearPurchased() {
+    this.items = this.items.filter(item => item.purchased === false)
+  }
+
+  get hasPurchasedItems() {
+    return this.items.filter(item => item.purchased).length > 0;
+  }
+
   markPurchased(item: ShoppingItem) {
     item.purchased = true;
+  }
+
+  addItem() {
+    const description = this.form.controls.item.value;
+    const newItem:ShoppingItem = {
+      description: description,
+      purchased: false
+    }
+
+    this.items = [newItem, ...this.items];
+
   }
 
 }
