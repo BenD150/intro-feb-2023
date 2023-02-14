c1bdc4f0148e5a42d640148a40febacaaf3a5b64
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-14 16:13:15 -0500

    Resources Controller

---
+++ Backend/LearningResourcesSolution/LearningResourcesApi/Controllers/ResourcesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LearningResourcesApi.Controllers;
+
+public class ResourcesController : ControllerBase
+{
+    [HttpGet("/resouces")]
+    public async Task<ActionResult> GetResources()
+    {
+        return Ok();
+    }
+ }
