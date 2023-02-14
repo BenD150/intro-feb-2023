7573d82ef47ed803748aaaf4a44b9201c58a7c79
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-14 16:36:08 -0500

    Configuring information from database

--- Backend/LearningResourcesSolution/LearningResourcesApi/Controllers/ResourcesController.cs
+++ Backend/LearningResourcesSolution/LearningResourcesApi/Controllers/ResourcesController.cs
@@ -1,12 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LearningResourcesApi.Adapters;
+using LearningResourcesApi.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LearningResourcesApi.Controllers;
 
 public class ResourcesController : ControllerBase
 {
-    [HttpGet("/resouces")]
+    private readonly LearningResourcesDataContext _context;
+
+    public ResourcesController(LearningResourcesDataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("/resources")]
     public async Task<ActionResult> GetResources()
     {
-        return Ok();
+        var items = await _context.Items
+            .Select(item => new GetResourceItem
+            {
+                Id = item.Id.ToString(),
+                Description = item.Description,
+                Link = item.Link,
+                Type = item.Type
+            }).ToListAsync();
+
+        var response = new GetResourcesResponse { Items = items };
+        return Ok(response);
     }
  }
