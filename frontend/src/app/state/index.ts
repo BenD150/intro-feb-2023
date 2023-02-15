5fb11e27723e1e1232c620a81e7b40593116577c
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-15 11:32:27 -0500

    select current counter

--- frontend/src/app/state/index.ts
+++ frontend/src/app/state/index.ts
@@ -1,9 +1,35 @@
-// The "Applciation State"
+// The "Application State"
 
-import { ActionReducerMap } from "@ngrx/store";
+import { ActionReducerMap, createFeatureSelector, createSelector } from "@ngrx/store";
+import * as fromCounter from './reducers/counter.reducer'
 
-export interface AppState { };
+export interface AppState {
+    counter: fromCounter.CounterState
+};
 
 
-// This object sets up the application state
-export const reducers: ActionReducerMap<AppState> = {}
+export const reducers: ActionReducerMap<AppState> = {
+    counter: fromCounter.reducer
+}
+
+
+// 1. Create a "feature selector"
+const selectCounterFeature = createFeatureSelector<AppState>("counter");
+
+
+// 2. Create a selector per branch of the state
+const selectCounterBranch = createSelector(
+    selectCounterFeature,
+    f => f.counter
+)
+
+
+// 3. Any helpers (optional)
+
+// 4. What does the component need
+// We need a selector that returns the current of the counter.
+
+export const selectCounterCurrent = createSelector(
+    selectCounterBranch,
+    (f => f.current)
+)
