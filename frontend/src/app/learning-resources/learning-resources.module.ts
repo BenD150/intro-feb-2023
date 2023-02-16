c919b5f40c337b1ee5fef86d8b7b6bba2c3c821a
Ben Dollenmayer <dollenmayer.8@osu.edu>
2023-02-16 11:20:02 -0500

    start of learning resources

---
+++ frontend/src/app/learning-resources/learning-resources.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LearningResourcesComponent } from './learning-resources.component';
+
+
+
+@NgModule({
+  declarations: [
+    LearningResourcesComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LearningResourcesModule { }
